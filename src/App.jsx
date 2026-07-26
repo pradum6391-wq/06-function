e438cbadf2872316f3a3a4c425113b37f79b593a
pradum6391-wq <pradum6391@gmail.com>
2026-07-26 22:46:53 +0530

    update app component with new function

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,37 @@ import React from 'react'
 
 const App = () => {
 
-  function btn(){
-    console.log("hello btn is clicked");
+  // function btn(){
+  //   console.log("hello btn is clicked");
+    
+  // }
+
+  // function mouseEnter(){
+  //      console.log("mouse enter");
+       
+  // }
+   
+  function inputChange(val){
+    console.log(val);
     
   }
+
   return (
     <div className='dv'>
-      <button onDoubleClick={btn}>Click here</button>
+      {/* <button onMouseEnter={mouseEnter} onDoubleClick={btn}>Click here</button> */}
+      <button onClick={function(){
+        console.log("btn is clicked");
+      }}>Click Here</button>
+
+      {/* <input onChange={(elem)=>{
+        console.log(elem.target.value);
+        
+      }} type="text" placeholder='enter your name' /> */}
+
+      <input onChange={(elem)=>{
+       inputChange(elem.target.value) 
+      }} type="text" placeholder='enter your name' />
+
     </div>
   )
 }
